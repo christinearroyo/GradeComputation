6e2a2194d63ad103402dcdffba2a7826b4715007
Christine Arroyo <chrstinearroyo@gmail.com>
2025-02-21 13:20:10 +0800

    Update Form1.cs

--- GradeComputation/Form1.cs
+++ GradeComputation/Form1.cs
@@ -12,18 +12,17 @@ namespace GradeComputation
         
         private void btnCompute_Click_1(object sender, EventArgs e)
         {
-            
-            double midtermQ = Convert.ToInt32(txtMidtermQ.Text);
-            double midtermExam = Convert.ToInt32(txtMidtermExam.Text);
-            double midtermCS = Convert.ToInt32(txtMidtermCS.Text);
+            int midtermQ = Convert.ToInt32(txtMidtermQ.Text);
+            int midtermExam = Convert.ToInt32(txtMidtermExam.Text);
+            int midtermCS = Convert.ToInt32(txtMidtermCS.Text);
 
-            double finalsQ = Convert.ToInt32(txtFinalsQ.Text);
-            double finalsExam = Convert.ToInt32(txtFinalsExam.Text);
-            double finalsCS = Convert.ToInt32(txtFinalsCS.Text);
+            int finalsQ = Convert.ToInt32(txtFinalsQ.Text);
+            int finalsExam = Convert.ToInt32(txtFinalsExam.Text);
+            int finalsCS = Convert.ToInt32(txtFinalsCS.Text);
 
             if (midtermQ < 50 || midtermExam < 50 || midtermCS < 50 || finalsQ < 50 || finalsExam < 50 || finalsCS < 50)
             {
-                MessageBox.Show("50 is the minimum value for each text box");
+                MessageBox.Show("50 is the minimum value for each text box.", "Invalid Input", MessageBoxButtons.OK, MessageBoxIcon.Information);
                 return;
             }
 
@@ -33,22 +32,22 @@ namespace GradeComputation
 
             string letterGrade;
 
-            if (semGrade >= 91)
+            if (semGrade >= 91.4)
             {
                 letterGrade = "A";
                 lblEquivGrade.Text = letterGrade;
             }
-            else if (semGrade >= 86 && semGrade <= 90)
+            else if (semGrade >= 86.5 && semGrade <= 90.4)
             {
                 letterGrade = "B";
                 lblEquivGrade.Text = letterGrade;
             }
-            else if (semGrade >= 81 && semGrade <= 85)
+            else if (semGrade >= 81.5 && semGrade <= 85.4)
             {
                 letterGrade = "C";
                 lblEquivGrade.Text = letterGrade;
             }
-            else if (semGrade >= 75 && semGrade <= 80)
+            else if (semGrade >= 75.5 && semGrade <= 80.4)
             {
                 letterGrade = "D";
                 lblEquivGrade.Text = letterGrade;
